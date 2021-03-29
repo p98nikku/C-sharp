cfe379acc579d69b77fd614306412865e74baf63
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 19:04:33 +0530

    An example on Polymorphism

---
+++ Lab-3/polystringoverload/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace polystringoverload
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            person obj = new person();
+            obj.InsertUserDetails("neetika", 22);
+            obj.ToString();
+          
+        }
+    }
+}
---
+++ Lab-3/polystringoverload/person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polystringoverload
+{
+    public class person
+    {
+        public string name;
+        public int age;
+        public void InsertUserDetails(string name, int age)
+        {
+
+            this.name = name;
+            this.age = age;
+
+        }
+
+        public string ToString()
+        {
+            string s = this.name + " is " + this.age + " year old";
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+}
