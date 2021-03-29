e31b2a071f11df818ba08b026d3809876e695a54
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 19:38:48 +0530

    A simple expression tree

---
+++ Lab-14/SimpleExpressionTree/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleExpressionTree
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Owner { get; set; }
+    }
+}
---
+++ Lab-14/SimpleExpressionTree/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleExpressionTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Title = "PEN",
+                    Price = 100,
+                    Owner = "Foo"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "PEN",
+                    Price = 400,
+                    Owner = "koo"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "PEN",
+                    Price = 100,
+                    Owner = "MOo"
+                }
+
+            };
+
+
+            var owners = products.Where(p => p.Price > 300).Select(p => new { p.Owner }).ToList();
+            owners.ForEach(o => Console.WriteLine(o));
+            var o = (from r in products where r.Price > 300 select new { r.Id, r.Owner }).ToList();
+            o.ForEach(i => Console.WriteLine(i));
+
+            //Console.WriteLine("Hello World!");
+            //Func<int, int> square = (x) => x * x;
+            //System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            //var a = e.Compile();
+            //Console.WriteLine(e);
+            //Console.WriteLine(a(5));
+        }
+    }
+}
+
