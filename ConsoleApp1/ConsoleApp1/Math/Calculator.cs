a5b8151c23cb52c9d08b50f4b1c4b7241686cd7a
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 18:37:59 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Math/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Math
+{
+    public class Calculator
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name" + FirstName + " " + LastName);
+        }
+    }
+}
