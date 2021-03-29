493372db437cdc901b3d6d0ccbac3ee652d38112
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 21:56:06 +0530

    Hello world

---
+++ Lab-1/Hello World/MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your Name: ");
+            var name =Console.ReadLine();
+            var date= DateTime.Now;
+            Console.WriteLine($" Hello {name} on {date}");
+            Console.ReadKey(true);  
+        }
+    }
+}
