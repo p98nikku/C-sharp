e438fa53fea4207e28da9d8e78c56f2937a8759e
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 19:16:17 +0530

    ExtensionMethod

---
+++ Lab-6/extensionmethod/extensionmeth.cs
@@ -0,0 +1,29 @@
+﻿using calculatorlib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extensionmethod
+{
+    public static class extensionmeth
+    {
+        public static int mul(this cal c)
+        {
+            return c.num1 * c.num2;
+        }
+        public static string str(this string s)
+        {
+            s = s.ToLower();
+            int v = 0;
+            for(int i=0;i<s.Length;i++)
+            {
+                if(s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='u'||s[i]=='i')
+                {
+                    v += 1;
+                }
+                  
+            }
+            return v;
+        }
+    }
+} 
---
+++ Lab-6/extmeth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using calculatorlib;
+using extensionmethod;
+namespace extmeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cal c = new cal
+            {
+                num1 = 9,
+                num2 = 8
+            };
+            var calc = c.mul();
+            Console.WriteLine(calc);
+
+            string ss = c("alphabet");
+
+            
+           
+        }
+      
+    }
+}
