93392f717adf1e80bd01a4daf4733087214e2067
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-29 21:54:54 +0530

    unit test(tdd)

---
+++ Lab-20/TDDdemo/CalculatorLib/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab-20/TDDdemo/CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5cd14a5d-7ffc-4d8c-9943-de5cf236fecb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
