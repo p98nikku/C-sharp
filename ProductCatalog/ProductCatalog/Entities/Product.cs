aaf54e44f0f3ca961a2b7797eb7764fac1a156d3
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-04-22 15:57:34 +0530

    updated entites

---
+++ ProductCatalog/ProductCatalog/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductCatalog.Entities
+{
+    public class Product
+    {
+        public static int AutoIncreament = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string ProductCategory { get; set; }
+        public string Description { get; set; }
+        public int SellingPrice { get; set; }
+        public string ShortCode { get; set; }
+
+        public Product()
+        {
+            AutoIncreament++;
+            Id = AutoIncreament;
+        }
+    }
+}
